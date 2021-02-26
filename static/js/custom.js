766ff8ca4199cea5cb9a0d8f93beb85e306c7e38
Tyler <cafesang161@gmail.com>
2021-02-26 09:17:00 +0700

    Using slick to implement carousel

---
+++ static/js/custom.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.slick-product').slick({
+        dots: false,
+        infinite: true,
+		arrows: true,
+        loop: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+});
